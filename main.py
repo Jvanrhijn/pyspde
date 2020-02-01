Fix variance on noise

main.py
@@ -6,44 +6,47 @@ from mpl_toolkits import mplot3d
 
 if __name__ == "__main__":
     coeff = 1
-    points = 20
-    steps = 100
+    points = 10
+    steps = 150**2
     tmax = 5
-    samples = 16
+    samples = 100
 
-    sigma = sqrt(0.75)
+    sigma = 1
     k = -1
 
     f = 1
-    g = lambda u: (k - sigma**2)*u;
+    g = lambda u: k*u
+    gderiv = lambda u: k
     #g = lambda u: -0.5*sigma*u*np.exp(-u**2)
 
     u0 = np.ones(points)
-    noise = WhiteNoise(sqrt(2), points)
+    noise = WhiteNoise(1, points)
 
     d1 = DerivativeOperator(1, 1/points, f, g)
+    d2 = DerivativeOperator(2, 1/points, f, g)
 
-    geometric_brownian = lambda a, t, x, w: -d1(a)**2/a + a*sigma*w(t, x)
-    linmult_arnold = lambda a, t, x, w: -d1(a)**2 * a/(1 + a**2) - (k - sigma**2)**2 * a / (1 + a**2) + sigma * np.sqrt(1 + a**2)*w(t, x)
-    linmult_graham = lambda a, t, x, w: -d1(a)**2 * a/(1 + a**2) - ((k - sigma**2)**2 - sigma**4/4) * a / (1 + a**2) + sigma * np.sqrt(1 + a**2)*w(t, x)
-    gaussian_arnold = lambda a, t, x, w: a*d1(a)**2 + sigma**4 / 4 * np.exp(-2*a**2) * (a**3 - 1.5*a) + sigma * np.exp(-a**2/2)*w(t, x)
+    linear = lambda a, t, w: -k**2 * a + sigma*w
+    geometric_brownian = lambda a, t, w: - d1(a)**2/a + a*sigma*w
+    linmult_arnold = lambda a, t, w: -d1(a)**2 * a/(1 + a**2) - (k - sigma**2)**2 * a / (1 + a**2) + sigma * np.sqrt(1 + a**2)*w
+    linmult_graham = lambda a, t, w: -d1(a)**2 * a/(1 + a**2) - ((k - sigma**2)**2 - sigma**4/4) * a / (1 + a**2) + sigma * np.sqrt(1 + a**2)*w
+    gaussian_arnold = lambda a, t, w: a*d1(a)**2 + sigma**4 / 4 * np.exp(-2*a**2) * (a**3 - 1.5*a) + sigma * np.exp(-a**2/2)*w
 
-    spde = SPDE(1, linmult_arnold, noise, points, f, g)
-    #spde = SPDE(1, lambda a, t, x, w: sigma*w(t, x), noise, points, f, g)
+    spde = SPDE(coeff, linmult_arnold, noise, points, f, g, right_deriv=gderiv)
 
-    solver = TrajectorySolver(spde, steps, tmax, u0, SpectralSolver)
+    solver = TrajectorySolver(spde, steps, tmax, u0, GalerkinSolver)
     ensemble_solver = EnsembleSolver(solver, samples)
     ensemble_solver.solve()
     mean = ensemble_solver.mean
     square = ensemble_solver.square
 
+    err = np.sqrt(square - mean**2)
     vis = Visualizer(mean, (0, tmax), (0, 1), error=ensemble_solver.sample_error)
     vis2 = Visualizer(square, (0, tmax), (0, 1), error=ensemble_solver.square_sample_error)
 
-    fig, ax = vis.surface()
+    fig, ax = vis.surface(cstride=10*points, rstride=10)
     ts = vis.xaxis
 
-    fig2, ax2 = vis.steady_state('o', label="Numerical solution")
+    fig2, ax2 = vis.steady_state(label="Numerical solution", marker='o', linestyle='-.')
     ax2.set_ylabel(r"$\langle\phi\rangle$")
     ax2.set_xlabel("t")
     ax2.plot(ts, np.exp(k*ts), label="Analytical; $B = \sigma\sqrt{1 + \phi^2}$")
@@ -65,7 +68,7 @@ if __name__ == "__main__":
 
     ax2.legend()
 
-    fig3, ax3 = vis2.steady_state('o', label="Numerical solution")
+    fig3, ax3 = vis2.steady_state('o', label="Numerical solution", marker='o', linestyle='-.')
     ax3.set_ylabel(r"$\langle\phi^2\rangle$")
     ax3.set_xlabel("t")
     ax3.plot(ts, 

src/linear_solvers.py
@@ -24,7 +24,7 @@ class GalerkinSolver(LinearSolver):
         """Store data and precompute lots of stuff"""
         self._xs = np.linspace(1/spde.points, 1, spde.points)
         self._spde = spde
-        self._gderiv = lambda u: (spde.right(u + 0.001) - spde.right(u)) / 0.001
+        self._gderiv = spde.right_deriv
         # assume sine basis, we'll generalize later
         self._basis_spectral = [partial(self._basis_sine, n) for n in range(1, spde.points)]\
             + [lambda x: x]
@@ -115,8 +115,8 @@ class SpectralSolver(LinearSolver):
         self._dt = dt
         self._transform = Transform([1, -1], spde.left, spde.right, spde.points)
         self._d = self._transform.derivative_matrix
-        self._d_inv = np.linalg.inv(self._d)
-        self._propagator = expm(-self._d*dt)
+        self._d_inv = 1/self._d
+        self._propagator = np.exp(-spde.linear*self._d*dt)
         self._xs = np.linspace(1/spde.points, 1, spde.points)
 
     def propagate_step(self, u):
@@ -138,9 +138,9 @@ class SpectralSolver(LinearSolver):
             theta_old = theta
 
             # Compute homogenized function in Fourier space using IP
-            vhat = (np.eye(dim) - propagator) @ self._d_inv @ theta + propagator @ v0hat
+            vhat = (1 - propagator) * self._d_inv * theta + propagator * v0hat
             # Compute time-derivative in Fourier space using the ODE for vhat
-            dvhat_dt = -self._d @ vhat + theta
+            dvhat_dt = -self._d * vhat + theta
 
             # Transform back to real space
             v = transform.ifft(vhat)
@@ -167,7 +167,7 @@ class SpectralSolver(LinearSolver):
 class Transform:
     """
     This class contains all logic required to perform an interaction picture
-    step. It contains code to transform the function to homogeneous boundaries,
+    step. It contains code to trsform the function to homogeneous boundaries,
     to compute the inhomogeneous function $\theta$, and to compute
     one fixed-point iteration of the boundary values.
     """
@@ -208,8 +208,7 @@ class Transform:
             else:
                 self._gderiv = gderiv
         offset = 0.5 if boundaries == [1, -1] else 0
-        self.derivative_matrix = np.diag(
-                ((np.arange(1, points+1) - offset)*np.pi)**2)
+        self.derivative_matrix = ((np.arange(1, points+1) - offset)*np.pi)**2
 
     def homogenize(self, u, x):
         """

src/noises.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
+from math import sqrt
 import copy
+
 import numpy as np
 from scipy.stats import norm
 
@@ -16,20 +18,19 @@ class WhiteNoise:
     def __init__(self, variance, dimension):
         self._variance = variance
         self._dimension = dimension
+        self._dx = 1/dimension
         self._value = np.zeros(dimension)
         self._time = 0.0
 
-    def __call__(self, t, xs):
+    def __call__(self, t):
         if t < self._time:
             raise ValueError("Can't compute white noise into the past")
         dt = t - self._time
-        # generate spatially delta-correlated white noise process
-        value_new = copy.deepcopy(self._value)
-        for j, x in enumerate(xs[1:]):
-            dx = x - xs[j]
-            value_new[j+1] = value_new[j] + norm.rvs(scale=self._variance*dx)\
-                * norm.rvs(scale=self._variance*dt)
-        self._value += value_new
+        # special case: if called at the same time point, return same value as previous
+        if dt == 0.0:
+            return self._value
+        # generate white noise process
+        self._value = norm.rvs(scale=sqrt(self._variance/(self._dx*dt)), size=self._dimension)
         self._time = t
         return self._value
 

src/spde.py
@@ -9,15 +9,24 @@ from scipy.linalg import expm
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 
+try:
+    import tqdm
+except ImportError:
+    tqdm = lambda s, *args, **kwargs: s
+
 
 class SPDE:
 
-    def __init__(self, linear, da, noise, points, left, right):
+    def __init__(self, linear, da, noise, points, left, right, right_deriv=None):
         self.noise = noise
         self.linear = linear
         self.da = da
         self.left = left
         self.right = right
+        if right_deriv is not None:
+            self.right_deriv = right_deriv
+        else:
+            self.right_deriv = lambda u: (right(u + 0.001) - right(u))/0.001
         self.points = points
 
 
@@ -38,16 +47,23 @@ class TrajectorySolver:
 
     def solve(self):
         noise = self._spde.noise
-        midpoints_iters = 3
+        midpoints_iters = 4
         for i in range(self._steps):
             # propagate solution to t + dt/2
-            a0 = self._linear_solver.propagate_step(self._solution[i])
+            if self._spde.linear != 0:
+                a0 = self._linear_solver.propagate_step(self._solution[i])
+            else:
+                a0 = self._solution[i]
             a = a0 
             # perform midpoint iteration
-            for it in range(midpoints_iters):
-                a = a0 + 0.5*self._dt * self._spde.da(a, self._time + 0.5*self._dt, self._xs, noise)
+            w = noise(self._time + 0.5*self._dt)
+            for _ in range(midpoints_iters):
+                a = a0 + 0.5*self._dt * self._spde.da(a, self._time + 0.5*self._dt, w)
             # propagate solution to t + dt
-            self._solution[i+1] = self._linear_solver.propagate_step(2*a - a0)
+            if self._spde.linear != 0.0:
+                self._solution[i+1] = self._linear_solver.propagate_step(2*a - a0)
+            else:
+                self._solution[i+1] = 2*a - a0
             self._time += self._dt
 
     @property
@@ -67,14 +83,14 @@ class EnsembleSolver:
         self.square_sample_error = None
 
     def solve(self):
-        for ensemble in range(self._ensembles):
+        for ensemble in tqdm.tqdm(range(self._ensembles)):
             self._trajectory_solvers[ensemble].solve()
             trajectory = self._trajectory_solvers[ensemble].solution
             self._storage[ensemble] = trajectory
         self.mean = np.mean(self._storage, axis=0)
         self.square = np.mean(self._storage**2, axis=0)
-        self.sample_error = np.std(self._storage, axis=0)
-        self.square_sample_error = np.std(self._storage**2, axis=0)
+        self.sample_error = np.std(self._storage, axis=0)/sqrt(self._ensembles - 1)
+        self.square_sample_error = np.std(self._storage**2, axis=0)/sqrt(self._ensembles - 1)
 
 
 class Visualizer:
@@ -118,15 +134,24 @@ class DerivativeOperator:
         self._dx = dx
         self._left = left
         self._right = right
+        du = 0.001
+        self._right_deriv = lambda u: (right(u+du) - right(u-du))/(2*du)
 
     def __call__(self, u):
         # TODO Improve this
         dim = len(u)
         matrix = np.zeros((dim, dim))
-        matrix[:-1, :-1] = np.diag(np.ones(dim-2), k=1) - np.diag(np.ones(dim-2), k=-1)
-        matrix[0, :] = matrix[1, :]
-        matrix[-1, -1] = 2*self._dx * self._right(u[-1]) / u[-1]
-        matrix[-2, -1] = 1
-        du = 1/(2*self._dx) * matrix @ u
+        if self._order == 1:
+            matrix[:-1, :-1] = np.diag(np.ones(dim-2), k=1) - np.diag(np.ones(dim-2), k=-1)
+            matrix[0, :] = matrix[1, :]
+            matrix[-1, -1] = 2*self._dx * self._right(u[-1]) / u[-1]
+            matrix[-2, -1] = 1
+            du = 1/(2*self._dx) * matrix @ u
+        elif self._order == 2:
+            raise NotImplementedError("Second-order FD not yet implemented")
+            du = np.zeros(dim)
+            du[1:-1] = (u[2:] - 2*u[1:-1] + u[:-2])/self._dx**2
+            du[0] = du[1]
+            du[-1] = 2 * (self._right(u[-1])/self._dx - (u[-1] - u[-2])/self._dx**2)
         return du
 