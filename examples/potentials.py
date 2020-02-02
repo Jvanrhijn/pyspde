Fix derivative operators in examples

examples/potentials.py
@@ -7,20 +7,20 @@ def linear(points, k, sigma, left, right):
 
 
 def geometric_brownian(points, k, sigma, left, right):
-    d1 = DerivativeOperator(1, points, left, right)
-    return lambda a, t, w: - d1(a)**2/a + a*sigma*w
+    d1 = DerivativeOperator(1, 1/points, left, right)
+    return lambda a, t, w: -d1(a)**2/a + a*sigma*w
 
 
 def linmult_arnold(points, k, sigma, left, right):
-    d1 = DerivativeOperator(1, points, left, right)
+    d1 = DerivativeOperator(1, 1/points, left, right)
     return lambda a, t, w: -d1(a)**2 * a/(1 + a**2) - (k - sigma**2)**2 * a / (1 + a**2) + sigma * np.sqrt(1 + a**2)*w
 
 
 def linmult_graham(points, k, sigma, left, right):
-    d1 = DerivativeOperator(1, points, left, right)
+    d1 = DerivativeOperator(1, 1/points, left, right)
     return lambda a, t, w: -d1(a)**2 * a/(1 + a**2) - ((k - sigma**2)**2 - sigma**4/4) * a / (1 + a**2) + sigma * np.sqrt(1 + a**2)*w
 
 
 def gaussian_arnold(points, k, sigma, left, right):
-    d1 = DerivativeOperator(1, points, left, right)
+    d1 = DerivativeOperator(1, 1/points, left, right)
     return lambda a, t, w: a*d1(a)**2 + sigma**4 / 4 * np.exp(-2*a**2) * (a**3 - 1.5*a) + sigma * np.exp(-a**2/2)*w

main.py
@@ -9,24 +9,22 @@ if __name__ == "__main__":
 
     coeff = 1
     points = 31
-    steps = 150
+    steps = 150**2
     tmax = 5
-    samples = 12
+    samples = 16
     processes = 4
 
     sigma = 0.25
     k = -1
 
     f = 1
-    def g(u): return (k - sigma**2)*u
-    def gderiv(u): return k - sigma**2
+    g = lambda u: (k - sigma**2)*u
+    gderiv = lambda u: k - sigma**2
     #g = lambda u: -0.5*sigma*u*np.exp(-u**2)
+    gderiv = None
 
     u0 = np.ones(points)
     noise = WhiteNoise(2, points)
-
-    d1 = DerivativeOperator(1, 1/points, f, g)
-    d2 = DerivativeOperator(2, 1/points, f, g)
 
     spde = SPDE(coeff, geometric_brownian(points, k, sigma, f, g),
                 noise, points, f, g, right_deriv=gderiv)
@@ -72,6 +70,7 @@ if __name__ == "__main__":
 
     def du(t, u):
         return np.vstack((u[1], u[0] / (1 + u[0]**2)*(u[1]**2 + (k - sigma**2)**2)))
+        #return np.vstack((u[1], u[0]*u[1]**2 + sigma**2 / 4 * np.exp(-2*u[0]**2) * (u[0]**3 - 1.5*u[0])))
 
     def bc(ua, ub):
         return np.array([ua[0] - f, ub[1] - g(ub[0])])

src/spde.py
@@ -231,8 +231,6 @@ class DerivativeOperator:
         self._dx = dx
         self._left = left
         self._right = right
-        du = 0.001
-        self._right_deriv = lambda u: (right(u+du) - right(u-du))/(2*du)
 
     def __call__(self, u):
         # TODO Improve this
@@ -244,7 +242,7 @@ class DerivativeOperator:
             matrix[0, :] = matrix[1, :]
             matrix[-1, -1] = 2*self._dx * self._right(u[-1]) / u[-1]
             matrix[-2, -1] = 1
-            du = 1/(2*self._dx) * matrix @ u
+            du = 1/(2*self._dx) * matrix @ u 
         elif self._order == 2:
             raise NotImplementedError("Second-order FD not yet implemented")
             du = np.zeros(dim)