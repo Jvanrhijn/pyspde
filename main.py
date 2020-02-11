9fd4a4e455e44e621b0476065ef53e9527ccd9be
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2020-02-11 14:03:01 +1100

    Clean up main.py

--- main.py
+++ main.py
@@ -29,9 +29,6 @@ if __name__ == "__main__":
 
     lattice = Lattice(0, 1, points, boundaries)
     basis = FiniteElementBasis(lattice, boundaries)
-    #basis = SpectralBasis(lattice, boundaries)
-
-    gderiv = None
 
     u0 = np.ones((1, points))
     noise = WhiteNoise(1, points) 
@@ -52,7 +49,6 @@ if __name__ == "__main__":
     )
 
     stepper = ThetaScheme(1, lattice, basis, tmax/steps)
-    #stepper = Midpoint(GalerkinSolver(problem), tmax/steps)
     #stepper = Midpoint(SpectralSolver(problem), tmax/steps)
 
     solver = TrajectorySolver(problem, steps, tmax, u0, stepper)
@@ -65,7 +61,6 @@ if __name__ == "__main__":
             observables={
                 "value": lambda x: x,
                 "square": lambda x: x**2,
-                #"energy": Integral(lambda x: 0.5*x**2, ts, 1),
             },
             blocks=blocks, 
             processes=processes, 
@@ -84,13 +79,6 @@ if __name__ == "__main__":
     step_errors = ensemble_solver.step_errors["value"]
     sample_errors = ensemble_solver.sample_errors["value"]
 
-    np.save("mean", mean)
-    np.save("square", square)
-    np.save("mean_step_error", step_errors)
-    np.save("mean_sample_erorr", sample_errors)
-    np.save("square_step_error", ensemble_solver.step_errors["square"])
-    np.save("square_sample_error", ensemble_solver.sample_errors["square"])
-
     print(f"Max step error =   {step_errors.max()}")
     print(f"Max sample error = {sample_errors.max()}")
 
@@ -105,8 +93,6 @@ if __name__ == "__main__":
     mesh_points = min([30, points, steps])
     fig, ax = vis.surface(cstride=points//mesh_points,
                           rstride=steps//mesh_points)
-    #fig, ax = vis.surface(cstride=1,
-    #                      rstride=30)
 
     fig2, ax2 = vis.steady_state(
         label="Numerical solution", marker='o', linestyle='-.')
@@ -122,28 +108,7 @@ if __name__ == "__main__":
     ax3.set_ylabel(r"$\langle\phi^2\rangle$")
     ax3.set_xlabel("t")
     ax3.plot(ts,
-             #f[field]**2*np.exp(2*k*ts),
              np.exp((2*k + sigma**2)*ts),
-    #         (f[field] + sigma**2/(2*k+sigma**2)) *
-    #         np.exp((2*k + sigma**2)*ts) - sigma**2/(2*k + sigma**2),
              label=r"Analytical solution")
 
-#
-#    """Now, let's compute the steady-state solution using Scipy"""
-#    from scipy.integrate import solve_bvp
-#
-#    def du(t, u):
-#        return np.vstack((u[1], u[0] / (1 + u[0]**2)*(u[1]**2 + (k - sigma**2)**2)))
-#        #return np.vstack((u[1], u[0]*u[1]**2 + sigma**2 / 4 * np.exp(-2*u[0]**2) * (u[0]**3 - 1.5*u[0])))
-#
-#    def bc(ua, ub):
-#        return np.array([ua[0] - f, ub[1] - g(ub[0])])
-#
-#    xs = np.linspace(0, 1, 100)
-#    ys = np.zeros((2, xs.size))
-#    res = solve_bvp(du, bc, xs, ys)
-#    ax2.plot(xs, res.sol(xs)[0], label="Scipy solution")
-#    ax3.plot(xs, res.sol(xs)[0]**2, label="Scipy solution")
-#    """End Scipy solution"""
-#
     plt.show()
